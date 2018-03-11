55eb0b7103f3124995a00e1b5673909c60b6e552
Jon Butler <burtnolej@googlemail.com>
2018-03-11 06:35:14 -0400

    tests work for process routines on windows

--- utils/misc_basic/misc_utils_process.py
+++ utils/misc_basic/misc_utils_process.py
@@ -2,10 +2,17 @@ from subprocess import Popen, STDOUT, PIPE
 from time import sleep
 import sys
 from misc_utils import thisfuncname
+from misc_utils_log import Log, logger, PRIORITY
 from os import remove, kill
 import signal
 import unittest
 
+if sys.platform == "win32":
+    LOGDIR = "./"
+else:
+    LOGDIR = "/tmp/log"
+
+log = Log(cacheflag=True,logdir=LOGDIR,verbosity=10)
 
 __all__ = ['process_start','process_stdin','process_kill','process_instances_get', \
            'process_get_stdout']
@@ -20,10 +27,10 @@ def process_start(cmdlineargs,stdin=True):
     
     p = Popen(cmdlineargs,**args)
 
-    from misc_utils_log import Log
-    log = Log(cacheflag=True,logdir="/tmp/log",verbosity=20,pidlogname=False,proclogname=False,reset="")
-    log.log(thisfuncname(),3,msg="creating process",cmdlineargs=cmdlineargs)
-    
+    #from misc_utils_log import Log
+    #log = Log(cacheflag=True,logdir="/tmp/log",verbosity=20,pidlogname=False,proclogname=False,reset="")
+    #log.log(thisfuncname(),3,msg="creating process",cmdlineargs=cmdlineargs)
+    log.log(PRIORITY.INFO,msg="creating process [" +  ",".join(cmdlineargs) + "]")
     return(p)
 
 def process_get_stdout(process):
@@ -37,39 +44,74 @@ def process_kill(p):
     ''' accepts a Popen object and if not assumes its a PID str or int'''
     
     if isinstance(p,Popen) == True:
-        _pid = p.pid
-        p.kill()
+
+        if sys.platform == "win32":
+            cmd = ['TASKKILL','/F','/T','/PID',str(p.pid)]
+            process_start(cmd)
+            return
+        else:
+            _pid = int(p.pid)
     else:
         try:
             _pid = int(p)
-            kill(_pid,signal.SIGTERM)
         except ValueError:
             raise Exception('requires an int or int as string')
 
+    kill(_pid,signal.SIGTERM)
     return()
     
 def process_instances_get(match):
 
-    cmd = ['ps','-ef']
+    '''
+    "Image Name","PID","Session Name","Session#","Mem Usage"\r\n
+    "System Idle Process","0","","0","8 K"\r\n
+    "System","4","","0","28 K"\r\n"smss.exe","344","","0","408 K"\r\n
+    ....
+    '''
+    
+    if sys.platform == "win32":
+        cmd = ['tasklist','/FO','csv']
+    else:
+        cmd = ['ps','-ef']
+        
     p = process_start(cmd)
     processlist = p.stdout.read()
     
-    # put back into string format for grep -v an remove blank last item 
-    process_str = "".join(list(processlist)[:-1]) 
+    if sys.platform == "win32":
+        plist = {}
+        for _pd in processlist.split("\n"):
+            try:
+                _pname = _pd.split(",")[0].replace("\"","")
+                _pid = int(_pd.split(",")[1].replace("\"",""))
+                if plist.has_key(_pname) == False:
+                    plist[_pname] = [_pid]
+                else:
+                    plist[_pname].append(_pid)
+            except:
+                #print "unable to split [",_pd,"]"
+                pass
+        
+        if plist.has_key(match):
+            return plist[match]
+        else:
+            return []
+            
+    else:
+        # put back into string format for grep -v an remove blank last item 
+        process_str = "".join(list(processlist)[:-1]) 
     
-    cmd = ['grep',match]
-    pgrep1 = process_start(cmd,stdin=True)
-    matches =  process_stdin(pgrep1,process_str)
-    #pgrep1.communicate(input=process_str)
+        cmd = ['grep',match]
+        pgrep1 = process_start(cmd,stdin=True)
+        matches =  process_stdin(pgrep1,process_str)
        
-    # put back into string format for grep -v an remove blank last item 
-    matches_str = "".join(list(matches)[:-1]) 
+        # put back into string format for grep -v an remove blank last item 
+        matches_str = "".join(list(matches)[:-1]) 
          
-    cmd = ['grep','-v','defunct']
-    pgrep2 = process_start(cmd,stdin=True)
+        cmd = ['grep','-v','defunct']
+        pgrep2 = process_start(cmd,stdin=True)
     
-    nondefunctmatches = process_stdin(pgrep2,matches_str)[0].split("\n")[:-1]
+        nondefunctmatches = process_stdin(pgrep2,matches_str)[0].split("\n")[:-1]
 
-    pid = [(match.split(" ")[2],match.split(" ")[4]) for match in nondefunctmatches]
+        pid = [(match.split(" ")[2],match.split(" ")[4]) for match in nondefunctmatches]
     
-    return(pid)
+        return(pid)
--- utils/misc_basic/test_misc_utils_process.py
+++ utils/misc_basic/test_misc_utils_process.py
@@ -2,34 +2,46 @@ from subprocess import Popen, STDOUT, PIPE
 from time import sleep
 import sys
 from misc_utils_process import process_instances_get, process_kill, \
-     process_start
+     process_start,process_get_stdout
 
 from misc_utils import Enum
 from image_utils import parse_convert_stdout
-from os import remove
+from os import remove, path
 import unittest
 
+
+IMDIR = "C:\Program Files (x86)\ImageMagick-6.7.5-Q8\\"
+
+class TestSubProcess_ls(unittest.TestCase):
+    # most basic exercising of process_start; 
+    def setUp(self):
+        pass
+
+    def test_(self):
+        cmd = ["dir",__file__] 
+        p = process_start(cmd)
+        self.assertEqual(path.basename(process_get_stdout(p).rstrip()),
+                         path.basename(__file__))
+    
 class TestSubProcess(unittest.TestCase):
             
     def setUp(self):
-        
         self.label = 'foobar'
         
     def test_success(self):
 
         # some external command to test with whose outputs are known
-        cmd = ['convert','-verbose','label:'+self.label,'-pointsize','22','/tmp/foobar.gif']
-        
+        cmd = [IMDIR+'convert','-verbose','label:'+self.label,'-pointsize','22','foobar.gif']
         p = process_start(cmd)
         status = parse_convert_stdout(p,self.label)
         
         self.assertEquals(status[0],0)
         self.assertTrue(status[1],'37x13')
-        self.assertTrue(status[2],'/tmp/foobar.txt')
+        self.assertTrue(status[2],'foobar.txt')
         
     def test_failure(self):
         # known external command --verbose is a bad switch
-        cmd = ['convert','--verbose','label:'+self.label,'-pointsize','22','/tmp/foobar.gif']
+        cmd = [IMDIR+'convert','--verbose','label:'+self.label,'-pointsize','22','foobar.gif']
         
         p = process_start(cmd)
         status = parse_convert_stdout(p,self.label)
@@ -37,12 +49,15 @@ class TestSubProcess(unittest.TestCase):
         
     def test_failure_grabstderr(self):
         # known external command --verbose is a bad switch
-        cmd = ['convert','--verbose','label:'+self.label,'-pointsize','22','/tmp/foobar.gif']
+        cmd = [IMDIR+'convert','--verbose','label:'+self.label,'-pointsize','22','foobar.gif']
         
         p = process_start(cmd)
         stderr = parse_convert_stdout(p,self.label)[1]
         
-        self.assertTrue(stderr.startswith('convert: unrecognized option'))
+        if sys.platform == "win32":
+            self.assertTrue(stderr.startswith('convert.exe: no decode delegate for this image format'))
+        else:     
+            self.assertTrue(stderr.startswith('convert: unrecognized option'))
         
     def test_killprocess(self):
         
@@ -50,32 +65,48 @@ class TestSubProcess(unittest.TestCase):
         import signal
         
         # some process that runs until killed
-        cmd = ['display','-geometry','750x750+7500+740','/tmp/foobar.gif']
+        if sys.platform == "win32":
+            cmd = ['cmd','/k','imdisplay','foobar.gif']
         
-        process_start(cmd)
-        pids=process_instances_get('display')
-        self.assertEquals(len(pids),1)
+            process_start(cmd)
+            
+            pids=process_instances_get('imdisplay.exe')
+            self.assertEquals(len(pids),1)
+            
+            for _pid in pids:
+                process_kill(_pid)
+                
+            pids=process_instances_get('imdisplay.exe')
+            self.assertEquals(len(pids),0)
+        else:
+            cmd = ['display','foobar.gif']
+            process_start(cmd)
+            
+            pids=process_instances_get('display')
+            self.assertEquals(len(pids),1)
+            
+            process_kill(pids[0][0])
+            pids=process_instances_get('display')
+            self.assertEquals(len(pids),0)
+            
         
-        process_kill(pids[0][0])
-        pids=process_instances_get('display')
-        self.assertEquals(len(pids),0)
-        
-    '''def test_killprocess_popen(self):
+    def test_killprocess_popen(self):
         # passing in a Popen object not a pid
         import os
         import signal
         
         # some process that runs until killed
-        cmd = ['display','-geometry','750x750+7500+740','/tmp/foobar.gif']
+        if sys.platform == "win32":
+            cmd = ['cmd','/k','imdisplay','foobar.gif']
+                    
+            p = process_start(cmd)
+            pids=process_instances_get('imdisplay.exe')
+            self.assertEquals(len(pids),1)
         
-        p = process_start(cmd)
-        pids=process_instances_get('display')
-        self.assertEquals(len(pids),1)
-        
-        process_kill(p)
-        pids=process_instances_get('display')
-        self.assertEquals(len(pids),0)
-        '''
+            process_kill(p)
+            sleep(1)
+            pids=process_instances_get('imdisplay.exe')
+            self.assertEquals(len(pids),0)
         
 class TestProcessUp(unittest.TestCase):
     
@@ -83,26 +114,28 @@ class TestProcessUp(unittest.TestCase):
         self.findpids=[]
         
         # some process that runs until killed
-        cmd = ['find','/','-name','foobar','-print']
+        cmd = ['cmd','/k','imdisplay','foobar.gif']
 
         for i in range(5):
             self.findpids.append(process_start(cmd))
 
     def test_processup(self):
         
-        pids=process_instances_get('find')
+        pids=process_instances_get('imdisplay.exe')
         self.assertEquals(len(pids),5)
         
     def tearDown(self):
         
         for p in self.findpids:
-            p.kill()
+            process_kill(p)
 
 
 if __name__ == "__main__":
 
     suite = unittest.TestSuite()
+    
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestSubProcess_ls))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestSubProcess))
-    #suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestProcessUp))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestProcessUp))
 
     unittest.TextTestRunner(verbosity=2).run(suite)
