bdae42a295c3e3bd87fa0482bce85d75d908880e
Jon Butler <burtnolej@googlemail.com>
2018-04-12 06:26:43 -0400

    added additional quad data accessors

--- app/quad/utils/data_utils.py
+++ app/quad/utils/data_utils.py
@@ -1,63 +1,129 @@
-from types import StringType, IntType
-from database_util import Database
-from database_table_util import tbl_query
+from types import StringType, IntType, ListType
+from utils.database.database_util import Database
+from utils.database.database_table_util import tbl_query
 
 __all__ = ["get_basic_student_info","get_basic_teacher_info",
            "get_student_schedule","get_teacher_schedule",
-           "get_students_per_class_by_teacher"]
+           "get_students_per_class_by_teacher",
+           "get_all_basic_student_info","get_all_basic_teacher_info",
+           "get_all_basic_course_info","get_basic_course_info",
+           "get_all_basic_subject_info","get_basic_subject_info",
+           "get_all_basic_prep_info","get_all_basic_timeperiod_info",
+           "get_all_basic_day_info"]
+
+def is_valid_course(course_id):
+    return True
+def is_valid_subject(subject_id):
+    return True
 
 def is_valid_student(student_id):
     return True
 def is_valid_teacher(teacher_id):
     return True
 
-def get_basic_student_info(database,student_id=70):
-    assert isinstance(student_id,IntType), student_id
-    assert is_valid_student(student_id), student_id
+def get_all_basic_student_info(database):
+    return get_basic_student_info(database, allstudents=True)
+
+def get_basic_student_info(database,students=[70],allstudents=False):
+    assert isinstance(students,ListType), students
+    assert is_valid_student(students), students
     assert isinstance(database,Database), database
     
-    sql = _qry_basic_student_info(student_id)
+    sql = _qry_basic_student_info(students,allstudents)
     with database:
-        _,results,_ = tbl_query(database,sql)
+        columns,results,_ = tbl_query(database,sql)
     
-    return results
+    return columns,results
+
+def get_all_basic_teacher_info(database):
+    return get_basic_teacher_info(database,allteachers=True)
     
-def get_basic_teacher_info(database,teacher_id=30):
-    assert isinstance(teacher_id,IntType), teacher_id
-    assert is_valid_teacher(teacher_id),teacher_id
+def get_basic_teacher_info(database,teachers=[30],allteachers=False):
+    assert isinstance(teachers,ListType), teachers
+    assert is_valid_teacher(teachers),teachers
     assert isinstance(database,Database), database
     
-    sql = _qry_basic_teacher_info(teacher_id)
+    sql = _qry_basic_teacher_info(teachers,allteachers)
     with database:
-        _,results,_ = tbl_query(database,sql)
+        columns,results,_ = tbl_query(database,sql)
     
-    return results
-    
-def get_student_schedule(database,studentid=70,
-                         days='"M","T","W","R","F"',
-                         periods='1,2,3,4,5,6,7,8,9,11'):
-    assert isinstance(studentid,IntType), studentid
-    assert is_valid_student(studentid), studentid
+    return columns,results
+
+def get_all_basic_prep_info(database):
     assert isinstance(database,Database), database
     
-    sql = _qry_student_schedule(studentid,days,periods)
+    sql = _qry_prep_info()
     with database:
-        _,results,_ = tbl_query(database,sql)
+        columns,results,_ = tbl_query(database,sql)
+    return columns,results
+
+def get_all_basic_timeperiod_info(database):
+    assert isinstance(database,Database), database
+    
+    sql = _qry_timeperiod_info()
+    with database:
+        columns,results,_ = tbl_query(database,sql)
+    return columns,results
+
+def get_all_basic_day_info(database):
+    assert isinstance(database,Database), database
+    
+    sql = _qry_day_info()
+    with database:
+        columns,results,_ = tbl_query(database,sql)
+    return columns,results
+
+def get_all_basic_course_info(database):
+    return get_basic_course_info(database,allcourses=True)
+    
+def get_basic_course_info(database,courses=[1],allcourses=False):
+    assert isinstance(courses,ListType), courses
+    assert is_valid_course(courses),courses
+    assert isinstance(database,Database), database
+    
+    sql = _qry_course_info(courses,allcourses)
+    with database:
+        columns,results,_ = tbl_query(database,sql)
+    return columns,results
+
+def get_all_basic_subject_info(database):
+    return get_basic_subject_info(database,allsubjects=True)
+    
+def get_basic_subject_info(database,subjects=[1],allsubjects=False):
+    assert isinstance(subjects,ListType), subjects
+    assert is_valid_course(subjects),subjects
+    assert isinstance(database,Database), subjects
+    
+    sql = _qry_subject_info(subjects,allsubjects)
+    with database:
+        columns,results,_ = tbl_query(database,sql)
+    return columns,results
+
+def get_student_schedule(database,students=[70],
+                         days=['"M"','"T"','"W"','"R"','"F"'],
+                         periods=[1,2,3,4,5,6,7,8,9,11]):
+    assert isinstance(students,ListType), students
+    assert is_valid_student(students), students
+    assert isinstance(database,Database), database
+    
+    sql = _qry_student_schedule(students,days,periods)
+    with database:
+        columns,results,_ = tbl_query(database,sql)
         
-    return results
+    return columns,results
     
-def get_teacher_schedule(database,teacher_id=3,
-                         days='"M","T","W","R","F"',
-                         periods='1,2,3,4,5,6,7,8,9,11'):
-    assert isinstance(teacher_id,IntType), teacher_id
-    assert is_valid_teacher(teacher_id),teacher_id
+def get_teacher_schedule(database,teachers=[3],
+                         days=['"M"','"T"','"W"','"R"','"F"'],
+                         periods=[1,2,3,4,5,6,7,8,9,11]):
+    assert isinstance(teachers,ListType), teachers
+    assert is_valid_teacher(teachers),teachers
     assert isinstance(database,Database), database
     
-    sql = _qry_teacher_schedule(teacher_id,days,periods)
+    sql = _qry_teacher_schedule(teachers,days,periods)
     with database:
-        _,results,_ = tbl_query(database,sql)
+        columns,results,_ = tbl_query(database,sql)
         
-    return results
+    return columns,results
         
 def get_students_per_class_by_teacher(database,teacher_id=3,
                                       class_ids='22,320'):
@@ -67,33 +133,77 @@ def get_students_per_class_by_teacher(database,teacher_id=3,
     
     sql = _qry_students_per_class_by_teacher(teacher_id,class_ids)
     with database:
-        _,results,_ = tbl_query(database,sql)
+        columns,results,_ = tbl_query(database,sql)
         
-    return results
-        
-def _qry_basic_student_info(student_id):
-    sql = ('select st.sStudentFirstNm, st.sStudentLastNm, st.idStudent, stl.idPrep, pc.sPrepNm '
-           'from Student st, StudentLevel stl, PrepCode pc '
-           'where st.idStudent = {} and st.cdRowStatus = "act" '
-           'and st.idStudent = stl.idStudent and stl.cdRowStatus = "act" and stl.idAcadPeriod = 1 '
-           'and stl.idPrep = pc.idPrep and pc.cdRowStatus = "act" ').format(student_id)
+    return columns,results
+
+
+def _qry_day_info():
+    sql = ('select idDay,sDayDesc,cdDay '
+           'from DayCode '
+           ' where cdRowStatus = "act" ')
     return sql
 
-def _qry_basic_teacher_info(teacher_id):
+def _qry_timeperiod_info():
+    sql = ('select idTimePeriod, dtPeriodStart, dtPeriodEnd '
+           'from TimePeriodCode '
+           ' where cdRowStatus = "act" ')
+    return sql
+
+def _qry_prep_info():
+    sql = ('select idPrep, sPrepNm '
+           'from PrepCode '
+           ' where cdRowStatus = "act" ')
+    return sql
+
+def _qry_subject_info(subjects,allsubjects=False):
+    sql = ('select sSubjectLongDesc,idSubject '
+           'from Subject '
+           ' where cdRowStatus = "act" ')
+           
+    if not allsubjects:
+        sql = sql + ('and idSubject in ({}) ').format(",".join(map(str,subjects)))
+    return sql
+           
+def _qry_course_info(courses,allcourses=False):
+    sql = ('select sCourseNm,idCourse,idSubject '
+           'from Course '
+           ' where cdRowStatus = "act" ')
+    
+    if not allcourses:
+        sql = sql + ('and idCourse in ({}) ').format(",".join(map(str,courses)))
+    return sql
+
+def _qry_basic_student_info(students,allstudents=False):
+    sql = ('select st.sStudentFirstNm, st.sStudentLastNm, st.idStudent, stl.idPrep, pc.sPrepNm '
+           'from Student st, StudentLevel stl, PrepCode pc '
+           'where st.cdRowStatus = "act" ')
+    
+    if not allstudents:
+        sql = sql + ('and st.idStudent in ({}) ').format(",".join(map(str,students)))
+    
+    sql = sql + ('and st.idStudent = stl.idStudent and stl.cdRowStatus = "act" and stl.idAcadPeriod = 1 '                 
+           'and stl.idPrep = pc.idPrep and pc.cdRowStatus = "act" ')
+    return sql
+
+def _qry_basic_teacher_info(teachers,allteachers=False):
     sql = ('select f.sFacultyFirstNm, f.sFacultyLastNm, f.idFaculty '
            'from Faculty f '
-           'where f.idFaculty = {} '
-           'and f.cdRowStatus = "act" '
-           'and f.cdEmployeeStatus = "act" ').format(teacher_id)
+           'where f.cdRowStatus = "act" ')
+    
+    if not allteachers:
+        sql = sql + ('and f.idFaculty in ({}) ').format(",".join(map(str,teachers)))
+        
+    sql = sql + ('and f.cdEmployeeStatus = "act" ')
     return sql
     
-def _qry_student_schedule(student_id,days,periods):
+def _qry_student_schedule(students,days,periods):
     sql = ('select sub.sSubjectLongDesc, c.sCourseNm, cls.sClassFocusArea, '
            '       f.sFacultyFirstNm, dc.cdDay, cl.idTimePeriod, cl.idLocation, '
            '       cl.idSection, ctc.cdClassType, s.iFreq, cl.idClassLecture '
            'from ClassLectureStudentEnroll cls, ClassLecture cl, DayCode dc, '
            '     Section s, Course c, Subject sub, Faculty f, ClassTypeCode ctc '
-           'where cls.idStudent = {} and cls.cdRowStatus = "act" '
+           'where cls.idStudent in ({}) and cls.cdRowStatus = "act" '
            'and dc.cdDay in ({}) '
            'and cl.idTimePeriod in ({}) '
            'and cls.idClassLecture = cl.idClassLecture and cl.cdRowStatus = "act" '
@@ -103,16 +213,16 @@ def _qry_student_schedule(student_id,days,periods):
            'and s.idSubject = sub.idSubject and sub.cdRowStatus = "act" '
            'and s.idLeadTeacher = f.idFaculty and f.cdRowStatus = "act" '
            'and s.idClassType = ctc.idClassType and ctc.cdRowStatus = "act" '
-           'order by cl.idDay, cl.idTimePeriod ').format(student_id,days,periods)
+           'order by cl.idDay, cl.idTimePeriod ').format(",".join(map(str,students)),",".join(map(str,days)),",".join(map(str,periods)))
     return sql
 
-def _qry_teacher_schedule(teacher_id,days,periods):
+def _qry_teacher_schedule(teachers,days,periods):
     sql = ('select sub.sSubjectLongDesc, c.sCourseNm, f.sFacultyFirstNm, dc.cdDay, '
            '       cl.idTimePeriod, cl.idLocation, cl.idSection, ctc.cdClassType, '
            '       s.iFreq, cl.idClassLecture '
            'from ClassLectureFacultyEnroll clf, ClassLecture cl, DayCode dc, Section s, Course c, '
            'Subject sub, Faculty f, ClassTypeCode ctc '
-           'where clf.idFaculty = {} and clf.cdRowStatus = "act" '
+           'where clf.idFaculty = ({}) and clf.cdRowStatus = "act" '
            'and dc.cdDay in ({}) '
            'and cl.idTimePeriod in ({}) '
            'and clf.idClassLecture = cl.idClassLecture and cl.cdRowStatus = "act" '
@@ -122,7 +232,7 @@ def _qry_teacher_schedule(teacher_id,days,periods):
            'and s.idSubject = sub.idSubject and sub.cdRowStatus = "act" '
            'and s.idLeadTeacher = f.idFaculty and f.cdRowStatus = "act" '
            'and s.idClassType = ctc.idClassType and ctc.cdRowStatus = "act" '
-           'order by cl.idDay, cl.idTimePeriod ').format(teacher_id,days,periods)
+           'order by cl.idDay, cl.idTimePeriod ').format(",".join(map(str,teachers)),",".join(map(str,days)),",".join(map(str,periods)))
     return sql
 
 def _qry_students_per_class_by_teacher(teacher_id,class_ids):
--- app/quad/utils/test_data_utils.py
+++ app/quad/utils/test_data_utils.py
@@ -1,8 +1,8 @@
 import unittest
 from os import path
-from data_utils import *
-from misc_utils import os_dir_exists
-from database_util import Database
+from app.quad.utils.data_utils import *
+from utils.misc_basic.misc_utils import os_dir_exists
+from utils.database.database_util import Database
 
 ROOTDIR = path.dirname(path.realpath(__file__))
 assert(os_dir_exists(ROOTDIR,"test_misc")) # test files go here
@@ -15,48 +15,128 @@ class Test_Base(unittest.TestCase):
     
     def tearDown(self):
         pass
+
+class Test_GetDayInfo(Test_Base):
+    def test_(self):
+        columns,results = get_all_basic_day_info(self.database)
+        self.assertEquals(len(results),5)
+
+class Test_GetTimePeriodInfo(Test_Base):
+    def test_(self):
+        columns,results = get_all_basic_timeperiod_info(self.database)
+        self.assertEquals(len(results),11)
+
+class Test_GetPrepInfo(Test_Base):
+    def test_(self):
+        columns,results = get_all_basic_prep_info(self.database)
+        self.assertEquals(len(results),9)
+        
+class Test_GetCourseInfo(Test_Base):
+    def test_(self):
+        expected_results =  [[u'Ancient Civilations', 1, 4]]
+        columns,results = get_basic_course_info(self.database)
+        self.assertEquals(results,expected_results)
+
+class Test_GetSubjectInfo(Test_Base):
+    def test_(self):
+        expected_results =   [[u'ELA', 1]]
+        columns,results = get_basic_subject_info(self.database)
+        self.assertEquals(results,expected_results)
     
+class Test_GetAllCourseInfo(Test_Base):
+    def test_(self):
+        columns,results = get_all_basic_course_info(self.database)
+        self.assertEquals(len(results),108)
+
+class Test_GetAllSubjectInfo(Test_Base):
+    def test_(self):
+        columns,results = get_all_basic_subject_info(self.database)
+        self.assertEquals(len(results),16)
+        
+class Test_GetAllBasicStudentInfo(Test_Base):
+    def test_(self):
+        expected_results = [[u'Donovan', u'Greene', 70, 7, u'Photon']]
+        columns,results = get_all_basic_student_info(self.database)
+        self.assertEqual(len(results),82)
+        
+        
 class Test_GetBasicStudentInfo(Test_Base):
     def test_(self):
         expected_results = [[u'Donovan', u'Greene', 70, 7, u'Photon']]
-        self.assertEquals(get_basic_student_info(self.database),expected_results)
-    
+        columns,results = get_basic_student_info(self.database)
+        self.assertEquals(results,expected_results)
+
+class Test_GetAllBasicStudentInfo(Test_Base):
+    def test_(self):
+        expected_results = [[u'Donovan', u'Greene', 70, 7, u'Photon']]
+        columns,results = get_all_basic_student_info(self.database)
+        self.assertEqual(len(results),82)
+        
+class Test_GetAllBasicTeacherInfo(Test_Base):
+    def test_(self):
+        expected_results = [[u'Nicole', u'Consaga', 30]]
+        columns,results = get_all_basic_teacher_info(self.database)
+        self.assertEqual(len(results),99)
+        
 class Test_GetBasicTeacherInfo(Test_Base):
     def test_(self):
         expected_results = [[u'Nicole', u'Consaga', 30]]
-        self.assertEquals(get_basic_teacher_info(self.database),expected_results)
+        columns,results = get_basic_teacher_info(self.database)
+        self.assertEquals(results,expected_results)
 
 class Test_GetStudentSchedule(Test_Base):
     def test_(self):
         expected_results = [[u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993]]
-        self.assertEquals(get_student_schedule(self.database,days='"M"',periods=1),expected_results)
+        columns,results = get_student_schedule(self.database,days=['"M"'],periods=[1])
+        self.assertEquals(results,expected_results)
         
 class Test_GetStudentScheduleArgs(Test_Base):
     def test_(self):
         expected_results = [[u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993]]
-        args = {'studentid':70,'days':'"M"','periods':1}
-        self.assertEquals(get_student_schedule(self.database,**args),expected_results)
+        args = {'students':[70],'days':['"M"'],'periods':[1]}
+        columns,results = get_student_schedule(self.database,**args)
+        self.assertEquals(results,expected_results)
 
 class TestGetTeacherSchedule(Test_Base):
     def test_(self):
         expected_results =  [[u'ELA', u'Weapons of Satire', u'Amelia', u'M', 2, 9, 110, u'Seminar', 2, 68]]
-        self.assertEquals(get_teacher_schedule(self.database,days='"M"',periods=2),expected_results)
+        columns,results = get_teacher_schedule(self.database,days=['"M"'],periods=[2])
+        self.assertEquals(results,expected_results)
 
 class TestGetStudentsPerClassByTeacher(Test_Base):
     def test_(self):
         expected_results = [[320, u'Julian', u'Krause', u'Weapons of Satire', None, u'M', 4, 27, 135, u'1on1', 2, 320]]
-        self.assertEquals(get_students_per_class_by_teacher(self.database,class_ids='320'),expected_results)
-
+        columns,results = get_students_per_class_by_teacher(self.database,class_ids='320')
+        self.assertEquals(results,expected_results)
+        
+class Test_GetStudentScheduleArgs_Columns(Test_Base):
+    """ test that the columns names are returned correctly
+    """
+    def test_(self):
+        expected_results = ['sSubjectLongDesc', 'sCourseNm', 'sClassFocusArea', 'sFacultyFirstNm', 'cdDay', 'idTimePeriod', 'idLocation', 'idSection', 'cdClassType', 'iFreq', 'idClassLecture']
+        args = {'students':[70],'days':['"M"'],'periods':[1]}
+        columns,results = get_student_schedule(self.database,**args)
+        self.assertEquals(columns,expected_results)
     
 if __name__ == "__main__":
     suite = unittest.TestSuite()   
     
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetStudentScheduleArgs))
-    
-    
-    '''suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetBasicStudentInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetBasicStudentInfo))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetBasicTeacherInfo))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetStudentSchedule))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestGetTeacherSchedule))
-    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestGetStudentsPerClassByTeacher))'''
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestGetStudentsPerClassByTeacher))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetStudentScheduleArgs_Columns))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetAllBasicStudentInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetAllBasicTeacherInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetCourseInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetSubjectInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetAllCourseInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetAllSubjectInfo))
+
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetPrepInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetDayInfo))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_GetTimePeriodInfo))
+
     unittest.TextTestRunner(verbosity=2).run(suite)    
