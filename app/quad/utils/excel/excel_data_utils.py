6f9b55607599beff51722d6a4a1179e877e0d91f
Jon Butler <burtnolej@googlemail.com>
2018-04-13 13:02:49 -0400

    excel_data_utils an tests updates that somehow were not on git

--- app/quad/utils/excel/excel_data_utils.py
+++ app/quad/utils/excel/excel_data_utils.py
@@ -1,20 +1,22 @@
 
-from data_utils import *
-from excel_utils import ExcelBase
-from misc_utils import os_dir_exists, encode, decode
-from xml_utils import xmlstr2dict
-from database_util import Database
+from app.quad.utils.data_utils import *
+from utils.excel.excel_utils import ExcelBase
+from utils.misc_basic.misc_utils import os_dir_exists, encode, decode, os_file_exists
+from utils.misc_basic.xml_utils import xmlstr2dict
+from utils.database.database_util import Database
 import sys
 from os import path
 from collections import OrderedDict
+from getopt import getopt, GetoptError, gnu_getopt
 
-from misc_utils_log import Log, logger, PRIORITY
+from utils.misc_basic.misc_utils_log import Log, logger, PRIORITY
 ROOTDIR = path.dirname(path.realpath(__file__))
 
 if sys.platform == "win32":
     LOGDIR = "./"
 else:
     LOGDIR = "/tmp/log"
+    
     
 log = Log(cacheflag=False,logdir=LOGDIR,verbosity=5)
 log.config =OrderedDict([('now',12),('type',10),('class',30),('funcname',30),
@@ -23,15 +25,27 @@ log.config =OrderedDict([('now',12),('type',10),('class',30),('funcname',30),
 __all__ = ['DataStoredProcBase','DataStoredProc']
 
 class DataStoredProcBase(ExcelBase):
-    def __init__(self,database_name,delete_flag=False,**kwargs):
+    def __init__(self,database_name,**kwargs):
         super(DataStoredProcBase,self).__init__(**kwargs)
         self.database_name = database_name
-        self.database = Database(self.database_name + ".sqlite",delete_flag)
+        self.database = Database(self.database_name + ".sqlite",False)
 
     @classmethod
     def _validate_database_name(self,encoding="unicode"):
         self._validate_field("database_name",encoding)
-      
+
+    @classmethod   
+    def _validate_header_flag(self,encoding="unicode"):
+        self._validate_flag("header_flag",encoding)
+        
+    @classmethod   
+    def _validate_runtime_dir(self,encoding="unicode"):
+        self._validate_filename("runtime_dir",encoding)
+    
+    @classmethod   
+    def _validate_result_file(self,encoding="unicode"):
+        self._validate_filename("result_file",encoding,mustexist=False)
+        
     @classmethod   
     def _validate_delete_flag(self,encoding="unicode"):
         self._validate_flag("delete_flag",encoding)
@@ -43,16 +57,16 @@ class DataStoredProcBase(ExcelBase):
         
         assert hasattr(self,'sp_name'), sp_name
         from sys import modules
-        from module_utils import _getmembers
+        from utils.misc_basic.module_utils import _getmembers
         
-        module = modules['data_utils']
+        module = modules['app.quad.utils.data_utils']
         module_members = _getmembers(module)
         
         sp_func_name = "get_" + self.sp_name
         if hasattr(module,sp_func_name) == False:
             return [-1]
         setattr(self,'sp_name',sp_func_name)
-        setattr(self,'sp_module','data_utils')
+        setattr(self,'sp_module','app.quad.utils.data_utils')
         
         log.log(PRIORITY.INFO,msg="located qry function; member attr sp_name set to ["+sp_func_name+"]")
             
@@ -63,7 +77,7 @@ class DataStoredProcBase(ExcelBase):
         """purpose: parse nvp's that need to be passed to the sp_func_name """
         assert hasattr(self,'sp_args'), sp_args
         import xml.etree.ElementTree as xmltree
-        valid_args = ['days','periods','teacherid','studentid']
+        valid_args = ['days','periods','teacchers','students']
         
         sp_args_dict = xmlstr2dict(decode(self.sp_args,encoding),doublequote=True)
         
@@ -76,12 +90,19 @@ class DataStoredProcBase(ExcelBase):
         return True
         
 class DataStoredProc(DataStoredProcBase):
+    
+    def __init__(self,database,**kwargs):
+        super(DataStoredProc,self).__init__(database,**kwargs)
+        # sp_args member attr is required even if empty
+        if hasattr(self,"sp_args") == False:
+            setattr(self,"sp_args",{})
+        
     @classmethod
     def stored_proc_by_file(cls,filepath,**kwargs):
         ''' query_str arg is encoded and is passed in a file'''        
         encoding = cls._get_file_encoding(filepath)
         cls._parse_input_file(filepath,mandatory_fields=['database_name','sp_name','delete_flag'],encoding=encoding,**kwargs)    
-        cls1 = cls(cls.database_name,cls.delete_flag,**kwargs)       
+        cls1 = cls(cls.database_name,**kwargs)       
         return(cls1._exec_stored_proc(encoding=encoding,**kwargs))
     
     def _exec_stored_proc(self,encoding="unicode",**kwargs):
@@ -89,48 +110,52 @@ class DataStoredProc(DataStoredProcBase):
         module = modules[self.sp_module]
         
         func = getattr(module,self.sp_name)
-        results = func(self.database,**self.sp_args)
-
+        columns,results = func(self.database,**self.sp_args)
+                    
         _result_file =self._get_parse_result_file(**kwargs)
-        
+
+        if hasattr(self,"header_flag"):
+            if getattr(self,"header_flag") == True:
+                results.insert(0,columns)
+                
         if _result_file != -1:
             self._create_output_file(_result_file,results)
-
+ 
         return(results)
 
-    def parse_args(argv):
+def parse_args(argv):
+    
+    mandatory_flags = ['--input_filename']
+    
+    try:
+        opts, args = getopt(argv[1:], "i:r:f:", ["input_filename=","result_file=","runtime_path=","="])
+        for flag in mandatory_flags:
+            if flag not in dict(opts).keys():
+                raise GetoptError(flag,"needs to be present")
+            
+        man_config = {} # mandatory flaags, hold the resulting, parsed flags
+        opt_config = {} # optional flags
         
-        mandatory_flags = ['--input_filename']
-        
-        try:
-            opts, args = getopt(argv[1:], "i:r:f:", ["input_filename=","result_file=","runtime_path=","="])
-            for flag in mandatory_flags:
-                if flag not in dict(opts).keys():
-                    raise GetoptError(flag,"needs to be present")
+        for flag, value in opts:
+            if flag in ("-i", "--input_filename") or flag in ("-r", "--runtime_path"):
+                if os_file_exists(value) == False:
+                    raise GetoptError(flag + "cannot find file" + value)
+            elif flag in ("-f", "--result_file"):
+                pass
+            else:
+                assert GetoptError(flag + " unhandled option")
                 
-            man_config = {} # mandatory flaags, hold the resulting, parsed flags
-            opt_config = {} # optional flags
-            
-            for flag, value in opts:
-                if flag in ("-i", "--input_filename") or flag in ("-r", "--runtime_path"):
-                    if os_file_exists(value) == False:
-                        raise GetoptError(flag + "cannot find file" + value)
-                elif flag in ("-f", "--result_file"):
-                    pass
-                else:
-                    assert GetoptError(flag + " unhandled option")
+            if flag in mandatory_flags:
+                man_config[flag.replace("--","")] = value
+            else:
+                opt_config[flag.replace("--","")] = value
                     
-                if flag in mandatory_flags:
-                    man_config[flag.replace("--","")] = value
-                else:
-                    opt_config[flag.replace("--","")] = value
-                        
-        except GetoptError as err:
-            print err
-            usage()
-            sys.exit(2)
-            
-        return man_config,opt_config
+    except GetoptError as err:
+        print err
+        usage()
+        sys.exit(2)
+        
+    return man_config,opt_config
     
 if __name__ == "__main__":
     
--- app/quad/utils/excel/test_excel_data_utils.py
+++ app/quad/utils/excel/test_excel_data_utils.py
@@ -1,9 +1,9 @@
 import unittest
 from os import path
-from data_utils import *
-from excel_data_utils import *
-from misc_utils import os_dir_exists, write_text_to_file, append_text_to_file, encode, os_file_delete, os_file_to_string, os_file_exists
-from database_util import Database
+from app.quad.utils.data_utils import *
+from app.quad.utils.excel.excel_data_utils import *
+from utils.misc_basic.misc_utils import os_dir_exists, write_text_to_file, append_text_to_file, encode, os_file_delete, os_file_to_string, os_file_exists
+from utils.database.database_util import Database
 from types import FunctionType, MethodType,StringType
 from sys import modules
 
@@ -21,10 +21,10 @@ class Test_DataStoredProc_validate_sp_name(unittest.TestCase):
         self.assertTrue(self.datasp._validate_sp_name())
         self.assertIsInstance(self.datasp.sp_name,StringType)
         
-        args = {'studentid':70,'days':'"M"','periods':1}
+        args = {'students':[70],'days':['"M"'],'periods':[1]}
         module = modules[self.datasp.sp_module]
         func = getattr(module,self.datasp.sp_name)
-        result = func(self.datasp.database,**args)
+        _,result = func(self.datasp.database,**args)
         
         self.assertEqual(result,[[u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993]])
         
@@ -48,7 +48,7 @@ class Test_DataStoredProc_StudentSchedule(unittest.TestCase):
         write_text_to_file(self.filename,"database_name:"+encode(path.join(TESTDIR,TESTDBNAME),self.encoding) + "\n")
         append_text_to_file(self.filename,"sp_name:"+encode("student_schedule",self.encoding) + "\n") 
         append_text_to_file(self.filename,"delete_flag:"+encode("False",self.encoding) + "\n") 
-        append_text_to_file(self.filename,"sp_args:"+encode("<root><studentid>70</studentid><days>M</days><periods>1</periods></root>",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"sp_args:"+encode("<root><students>70</students><days>M</days><periods>1</periods></root>",self.encoding) + "\n") 
         
         result = DataStoredProc.stored_proc_by_file(self.filename,result_file=self.result_filename)
         self.assertEqual(result,[[u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993]])
@@ -58,12 +58,76 @@ class Test_DataStoredProc_StudentSchedule(unittest.TestCase):
         
     def tearDown(self):
         os_file_delete(self.filename)
+                       
+class Test_DataStoredProc_StudentSchedule_MultiVals(unittest.TestCase):
+    def setUp(self):
+        self.filename = "uupyshell.txt"
+        self.result_filename = "C:\\Users\\burtnolej\\pyshell_result.txt"
+        self.datasp = DataStoredProc(path.join(TESTDIR,TESTDBNAME)) 
+        self.encoding = "uu"
+            
+    def test_(self):
+        
+        write_text_to_file(self.filename,"database_name:"+encode(path.join(TESTDIR,TESTDBNAME),self.encoding) + "\n")
+        append_text_to_file(self.filename,"sp_name:"+encode("student_schedule",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"delete_flag:"+encode("False",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"sp_args:"+encode("<root><students>70</students><students>69</students><days>M</days><days>T</days><periods>1</periods><periods>2</periods></root>",self.encoding) + "\n") 
+        
+        result = DataStoredProc.stored_proc_by_file(self.filename,result_file=self.result_filename)
+        
+        expected_results= [[u'Homeroom', u'Homeroom', None, u'Samantha', u'M', 1, 10, 163, u'Seminar', 5, 983], 
+                           [u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993], 
+                           [u'Movement', u'Movement', None, u'Michael', u'M', 2, 37, 432, u'Seminar', 1, 1196], 
+                           [u'Science', u'Science', None, u'John', u'M', 2, 4, 162, u'1on1', 1, 143], 
+                           [u'Homeroom', u'Homeroom', None, u'Samantha', u'T', 1, 10, 163, u'Seminar', 5, 984], 
+                           [u'Homeroom', u'Homeroom', None, u'Isaac', u'T', 1, 9, 165, u'Seminar', 5, 994], 
+                           [u'Foreign Language', u'Spanish 1', None, u'Jamie', u'T', 2, 6, 451, u'Seminar', 2, 1218], 
+                           [u'Work Period', u'Work Period', None, u'Alyssa', u'T', 2, 37, 564, u'Seminar', 1, 713]]
+        
+        
+        self.assertEqual(result,expected_results)
 
+        self.assertTrue(os_file_exists(self.result_filename))
+        self.assertEqual(os_file_to_string(self.result_filename),"Homeroom^Homeroom^None^Samantha^M^1^10^163^Seminar^5^983$$Homeroom^Homeroom^None^Isaac^M^1^9^165^Seminar^5^993$$Movement^Movement^None^Michael^M^2^37^432^Seminar^1^1196$$Science^Science^None^John^M^2^4^162^1on1^1^143$$Homeroom^Homeroom^None^Samantha^T^1^10^163^Seminar^5^984$$Homeroom^Homeroom^None^Isaac^T^1^9^165^Seminar^5^994$$Foreign Language^Spanish 1^None^Jamie^T^2^6^451^Seminar^2^1218$$Work Period^Work Period^None^Alyssa^T^2^37^564^Seminar^1^713")
+        
+    def tearDown(self):
+        os_file_delete(self.filename)
+
+class Test_DataStoredProc_StudentScheduleWithHeaders(unittest.TestCase):
+    def setUp(self):
+        self.filename = "uupyshell.txt"
+        self.result_filename = "C:\\Users\\burtnolej\\pyshell_result.txt"
+        self.datasp = DataStoredProc(path.join(TESTDIR,TESTDBNAME)) 
+        self.encoding = "uu"
+            
+    def test_(self):
+        
+        write_text_to_file(self.filename,"database_name:"+encode(path.join(TESTDIR,TESTDBNAME),self.encoding) + "\n")
+        append_text_to_file(self.filename,"sp_name:"+encode("student_schedule",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"delete_flag:"+encode("False",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"header_flag:"+encode("True",self.encoding) + "\n") 
+        append_text_to_file(self.filename,"sp_args:"+encode("<root><students>70</students><days>M</days><periods>1</periods></root>",self.encoding) + "\n") 
+        
+        result = DataStoredProc.stored_proc_by_file(self.filename,result_file=self.result_filename)
+        self.assertEqual(result,[['sSubjectLongDesc', 'sCourseNm', 'sClassFocusArea', 'sFacultyFirstNm', 'cdDay', 'idTimePeriod', 'idLocation', 'idSection', 'cdClassType', 'iFreq', 'idClassLecture'], [u'Homeroom', u'Homeroom', None, u'Isaac', u'M', 1, 9, 165, u'Seminar', 5, 993]])
+        
+        self.assertTrue(os_file_exists(self.result_filename))
+        self.assertEqual("sSubjectLongDesc^sCourseNm^sClassFocusArea^sFacultyFirstNm^cdDay^idTimePeriod^idLocation^idSection^cdClassType^iFreq^idClassLecture$$Homeroom^Homeroom^None^Isaac^M^1^9^165^Seminar^5^993",
+        os_file_to_string(self.result_filename))
+        
+    def tearDown(self):
+        os_file_delete(self.filename)
+        
 if __name__ == "__main__":
     suite = unittest.TestSuite()   
     
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_DataStoredProc_validate_sp_name))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_DataStoredProc_validate_sp_args))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_DataStoredProc_StudentSchedule))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_DataStoredProc_StudentSchedule_MultiVals))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_DataStoredProc_StudentScheduleWithHeaders))
+    
+    
+    
     
     unittest.TextTestRunner(verbosity=2).run(suite)
